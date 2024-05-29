c124185fea1e20addb20718bd711ee7c168a228c
REHANTAHIR111 <158707821+REHANTAHIR111@users.noreply.github.com>
2024-05-29 06:52:52 -0700

    Update:

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -6,9 +6,9 @@ export class NewsItem extends Component {
     let { title, description, imageUrl, newsUrl, author, date, source } = this.props
     return (
       <div>
-        <div className="card shadow-xl rounded">
+        <div className="card shadow-xl rounded relative overflow-hidden bg-cover bg-no-repeat">
           <span class="position-absolute z-1 top-0 left-[90%] font-semibold translate-middle badge rounded-pill bg-danger">{source}</span>
-          <img src={!imageUrl ? "https://cdn.zeebiz.com/sites/default/files/styles/zeebiz_850x478/public/2019/02/15/74385-hacker-pixabay.jpg?itok=raw77Ci5&c=dc4689881a8e4678456af987aba25e8f" : imageUrl} />
+          <img className='transition-all duration-500 hover:scale-110' src={!imageUrl ? "https://cdn.zeebiz.com/sites/default/files/styles/zeebiz_850x478/public/2019/02/15/74385-hacker-pixabay.jpg?itok=raw77Ci5&c=dc4689881a8e4678456af987aba25e8f" : imageUrl} />
           <div className="card-body">
             <h2 className='font-semibold text-lg card-title'>{title}</h2>
             <p className="card-text">{description}...</p>
